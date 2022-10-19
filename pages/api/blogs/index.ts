a045e5b09d77f200d8d67c5190dce4b66d44b274
wjdghks963 <chsw000@naver.com>
2022-10-19 11:28:06 +0900

    bugfix : - data null or data done return nothing fixed

--- pages/api/blogs/index.ts
+++ pages/api/blogs/index.ts
@@ -1,49 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prismaclient from "@libs/server/prismaClient";
 
-type PagenationQuery = { page?: string; limit?: string; cursor?: string };
+type PagenationQuery = { tag?: string; limit?: string; cursor?: string };
 
 export default async function Blogs(req: NextApiRequest, res: NextApiResponse) {
-  const { page, limit, cursor }: PagenationQuery = req.query;
-
-  const firstPost = await prismaclient.post.findMany({
-    take: limit ? +limit : 5,
-    orderBy: {
-      createdAt: "desc",
-    },
-    include: {
-      tags: {
-        select: {
-          tag: true,
-        },
-      },
-    },
-  });
-
-  const firstCursor = firstPost[firstPost.length - 1].id;
-
-  // const posts = await prismaclient.post.findMany({
-  //   take: limit ? +limit : 5,
-  //   skip: 1,
-  //   cursor: {
-  //     id: +firstCursor,
-  //   },
-  //   orderBy: {
-  //     createdAt: "desc",
-  //   },
-  //   select: {
-  //     tags: true,
-  //   },
-  // });
-  // console.log(posts);
-
-  if (cursor) {
-    const posts = await prismaclient.post.findMany({
+  const { tag, limit, cursor }: PagenationQuery = req.query;
+  console.log(tag);
+  if (tag === undefined) {
+    const firstPost = await prismaclient.post.findMany({
       take: limit ? +limit : 5,
-      skip: 1,
-      cursor: {
-        id: +cursor,
-      },
       orderBy: {
         createdAt: "desc",
       },
@@ -56,10 +21,89 @@ export default async function Blogs(req: NextApiRequest, res: NextApiResponse) {
       },
     });
 
-    const nextCursor = posts.length !== 0 ? posts[posts.length - 1].id : "done";
+    const firstCursor =
+      firstPost.length === 0 ? "done" : firstPost[firstPost.length - 1].id;
 
-    return res.status(200).json({ data: posts, nextCursor });
+    if (cursor) {
+      const posts = await prismaclient.post.findMany({
+        take: limit ? +limit : 5,
+        skip: 1,
+        cursor: {
+          id: +cursor,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+        include: {
+          tags: {
+            select: {
+              tag: true,
+            },
+          },
+        },
+      });
+
+      const nextCursor =
+        posts.length !== 0 && posts.length === +limit!
+          ? posts[posts.length - 1].id
+          : "done";
+
+      return res.status(200).json({ data: posts, nextCursor });
+    }
+
+    return res.status(200).json({ data: firstPost, nextCursor: firstCursor });
+  } else if (tag) {
+    const firstPost = await prismaclient.post.findMany({
+      take: limit ? +limit : 5,
+      orderBy: {
+        createdAt: "desc",
+      },
+      where: {
+        tags: { some: { tag } },
+      },
+      include: {
+        tags: {
+          select: {
+            tag: true,
+          },
+        },
+      },
+    });
+    const firstCursor =
+      firstPost.length !== 0 && firstPost.length === +limit!
+        ? firstPost[firstPost.length - 1].id
+        : "done";
+
+    if (cursor) {
+      const posts = await prismaclient.post.findMany({
+        take: limit ? +limit : 5,
+        skip: 1,
+        cursor: {
+          id: +cursor,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+        where: {
+          tags: { some: { tag } },
+        },
+        include: {
+          tags: {
+            select: {
+              tag: true,
+            },
+          },
+        },
+      });
+
+      const nextCursor =
+        posts.length !== 0 && posts.length === +limit!
+          ? posts[posts.length - 1].id
+          : "done";
+
+      return res.status(200).json({ data: posts, nextCursor });
+    }
+
+    return res.status(200).json({ data: firstPost, nextCursor: firstCursor });
   }
-
-  return res.status(200).json({ data: firstPost, nextCursor: firstCursor });
 }
