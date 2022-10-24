3f92ab7c1b4f112669d55039fb4a70f7385687fd
wjdghks963 <chsw000@naver.com>
2022-10-24 15:56:30 +0900

    none

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,14 +2,13 @@ import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
 export default NextAuth({
-  site: process.env.NEXTAUTH_URL,
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
-  session: {},
+  session: { strategy: "jwt" },
   secret: process.env.SECRET,
   callbacks: {
     async jwt({ token, account }) {
@@ -21,6 +20,9 @@ export default NextAuth({
     async session({ session, token, user }) {
       session.accessToken = token.accessToken;
       return session;
+    },
+    async redirect({ url, baseUrl }) {
+      return url;
     },
   },
 });
