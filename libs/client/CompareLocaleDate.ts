d5ae407bc536a13fca3da934ab54aaaaaafb0c5d
wjdghks963 <chsw000@naver.com>
2022-10-28 14:49:48 +0900

    test : - modified date func for test

--- libs/client/CompareLocaleDate.ts
+++ libs/client/CompareLocaleDate.ts
@@ -2,13 +2,22 @@ const compareLocaleDate = (
   create: Date | string,
   update: Date | string
 ): string => {
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
   if (create !== update) {
-    const convert = new Date(update).toLocaleDateString();
-    return convert.substring(0, convert.length - 1);
+    const convert = new Date(update).toLocaleDateString(
+      "ko-KR",
+      options as any
+    );
+    return convert;
   }
 
-  const convert = new Date(create).toLocaleDateString();
-  return convert.substring(0, convert.length - 1);
+  const convert = new Date(create).toLocaleDateString("ko-KR", options as any);
+  return convert;
 };
 
 export default compareLocaleDate;
