ba195893b0b679f019e7f01b160fee6bc8d1c064
wjdghks963 <chsw000@naver.com>
2023-01-15 21:24:07 +0900

    added : query api added

--- pages/api/blogs/index.ts
+++ pages/api/blogs/index.ts
@@ -1,12 +1,36 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prismaclient from "@libs/server/prismaClient";
 
-type PagenationQuery = { tag?: string; limit?: string; cursor?: string };
+type SearchQuery = "" | string;
+type Tag = "all" | string;
+
+type PagenationQuery = {
+  tag?: Tag;
+  query?: SearchQuery;
+  limit?: string;
+  cursor?: string;
+};
+
+/**
+ *
+ * @param posts DB가 찾은 포스트들
+ * @param limit uri query로 들어온 요청 -> 갯수 몇개?
+ * @returns cursor가 끝인지 "done" 아니라면 뒤에 몇번째를 가져와야하나
+ */
+const getCursor = (posts: any, limit: number) => {
+  return posts.length !== 0 && posts.length === limit!
+    ? posts[posts.length - 1].id
+    : "done";
+};
 
 export default async function Blogs(req: NextApiRequest, res: NextApiResponse) {
-  const { tag, limit, cursor }: PagenationQuery = req.query;
+  const { tag, query, limit, cursor }: PagenationQuery = req.query;
 
-  if (tag === "all") {
+  /**
+   * tag가 전체이거나 query가 빈문자열일때 모든 포스트들을 보여줌
+   * else if => tag가 all이 아니거나
+   */
+  if (tag === "all" || query === "") {
     const firstPost = await prismaclient.post.findMany({
       take: limit ? +limit : 5,
       orderBy: {
@@ -21,8 +45,8 @@ export default async function Blogs(req: NextApiRequest, res: NextApiResponse) {
       },
     });
 
-    const firstCursor =
-      firstPost.length === 0 ? "done" : firstPost[firstPost.length - 1].id;
+    const firstCursor = getCursor(firstPost, +limit!!);
+    //firstPost.length === 0 ? "done" : firstPost[firstPost.length - 1].id;
 
     if (cursor) {
       const posts = await prismaclient.post.findMany({
@@ -43,16 +67,13 @@ export default async function Blogs(req: NextApiRequest, res: NextApiResponse) {
         },
       });
 
-      const nextCursor =
-        posts.length !== 0 && posts.length === +limit!
-          ? posts[posts.length - 1].id
-          : "done";
+      const nextCursor = getCursor(posts, +limit!!);
 
       return res.status(200).json({ data: posts, nextCursor });
     }
 
     return res.status(200).json({ data: firstPost, nextCursor: firstCursor });
-  } else if (tag) {
+  } else if (tag !== "all" && tag !== undefined) {
     const firstPost = await prismaclient.post.findMany({
       take: limit ? +limit : 5,
       orderBy: {
@@ -69,10 +90,7 @@ export default async function Blogs(req: NextApiRequest, res: NextApiResponse) {
         },
       },
     });
-    const firstCursor =
-      firstPost.length !== 0 && firstPost.length === +limit!
-        ? firstPost[firstPost.length - 1].id
-        : "done";
+    const firstCursor = getCursor(firstPost, +limit!!);
 
     if (cursor) {
       const posts = await prismaclient.post.findMany({
@@ -96,10 +114,62 @@ export default async function Blogs(req: NextApiRequest, res: NextApiResponse) {
         },
       });
 
-      const nextCursor =
-        posts.length !== 0 && posts.length === +limit!
-          ? posts[posts.length - 1].id
-          : "done";
+      const nextCursor = getCursor(posts, +limit!!);
+
+      return res.status(200).json({ data: posts, nextCursor });
+    }
+
+    return res.status(200).json({ data: firstPost, nextCursor: firstCursor });
+  }
+
+  if (query !== "" && query !== undefined) {
+    const firstPost = await prismaclient.post.findMany({
+      take: limit ? +limit : 5,
+      orderBy: {
+        createdAt: "desc",
+      },
+      where: {
+        title: {
+          contains: String(query),
+        },
+      },
+      include: {
+        tags: {
+          select: {
+            tag: true,
+          },
+        },
+      },
+    });
+    console.log("first" + firstPost);
+
+    const firstCursor = getCursor(firstPost, +limit!!);
+
+    if (cursor) {
+      const posts = await prismaclient.post.findMany({
+        take: limit ? +limit : 5,
+        skip: 1,
+        cursor: {
+          id: +cursor,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+        where: {
+          title: {
+            contains: String(query),
+          },
+        },
+        include: {
+          tags: {
+            select: {
+              tag: true,
+            },
+          },
+        },
+      });
+
+      const nextCursor = getCursor(posts, +limit!!);
 
       return res.status(200).json({ data: posts, nextCursor });
     }
