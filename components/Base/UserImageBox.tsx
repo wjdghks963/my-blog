5b622f45a9f45347df01e7ef7bafc3b190d52f27
wjdghks963 <chsw000@naver.com>
2023-05-08 22:46:06 +0900

    modified : - 도메인과 oauth의 프로필 이미지 컴포넌트

--- components/Base/UserImageBox.tsx
+++ components/Base/UserImageBox.tsx
@@ -1,8 +1,23 @@
 import Image from "next/image";
 import React from "react";
+import {getDomainFromImageUrl} from "@libs/client/getDomainImg";
+import {cls} from "@libs/client/utils";
 
-export default function UserImageBox({src}:{src:string}){
+
+
+
+export default function UserImageBox({src,className}:{src:string,className?:string}){
+
+    const imgURL = getDomainFromImageUrl(src) as string;
+
     return (
-        <Image className={'rounded-3xl ring-2 ring-gray-300 ring-offset-2'} src={src} width={100} height={100}/>
+        <div className={cls(className ? className : '',"relative")}>
+            <Image className={'rounded-2xl w-full h-full'} src={src} width={100} height={100}/>
+            <div className={"absolute rounded-full bg-white block -top-2 -left-2 flex justify-center "}>
+                <Image className={' w-8 h-8'} src={imgURL} width={25} height={25}/>
+            </div>
+        </div>
     )
 }
+
+
