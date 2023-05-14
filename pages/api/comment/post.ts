de00f607ff7d8c39a5fb2a7bcfaa077bd0788477
wjdghks963 <chsw000@naver.com>
2023-05-14 15:55:05 +0900

    modified : - type 추가

--- pages/api/comment/post.ts
+++ pages/api/comment/post.ts
@@ -1,16 +1,24 @@
 import prismaclient from "@libs/server/prismaClient";
 import type { NextApiRequest, NextApiResponse } from "next";
 
+
+export interface CommentPostJson {
+    postId:string,
+    userEmail:string,
+    content:string
+}
+
 export default async function post(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    const { postId, userId, content }:{postId:string, userId:string, content:string} = req.body;
+    const { postId, userEmail, content }:CommentPostJson= req.body;
+
     try {
         const comment = await prismaclient.comment.create({
             data: {
                 content,
-                user: { connect: { id: +userId } },
+                user: { connect: { email: userEmail } },
                 post: { connect: { id: +postId } },
             },
         });
