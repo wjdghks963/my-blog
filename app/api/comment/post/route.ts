a8d697ee9fb5b84b3b3aeb5137eba189eb5b53e9
wjdghks963 <chsw000@naver.com>
2023-05-23 21:54:48 +0900

    modified : - comment post api 13에 맞게 완성

---
+++ app/api/comment/post/route.ts
@@ -0,0 +1,28 @@
+import prismaclient from "@libs/server/prismaClient";
+import {CommentPostJson} from '@types'
+import {NextResponse} from 'next/server'
+
+
+
+
+export async function POST(
+    req: Request,
+) {
+
+    const { postId, userEmail, content }:CommentPostJson = await req.json();
+
+    try {
+        const comment = await prismaclient.comment.create({
+            data: {
+                content,
+                user: { connect: { email: userEmail } },
+                post: { connect: { id: +postId } },
+            },
+        });
+        return NextResponse.json({comment})
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ message: 'Failed to create comment.' });
+    }
+
+}
