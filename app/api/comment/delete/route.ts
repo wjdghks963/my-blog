837b96136662b7c4b691155a82bde8c359899c31
wjdghks963 <chsw000@naver.com>
2023-05-24 22:07:26 +0900

    modified : - comment api app dir 맞춰 변경

---
+++ app/api/comment/delete/route.ts
@@ -0,0 +1,25 @@
+import prismaclient from "@libs/server/prismaClient";
+import {Request} from 'next/dist/compiled/@edge-runtime/primitives/fetch'
+import {NextResponse} from 'next/server'
+
+export async function POST(
+    req: Request
+) {
+
+    const {id} = await req.json();
+console.log('delete',id)
+    try {
+        if (id) {
+            const comment = await prismaclient.comment.delete({
+                where: {id: +id},
+            });
+            return NextResponse.json(comment)
+        } else {
+            return NextResponse.json({message: 'Comment Not Found'})
+        }
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({message: 'Failed to delete comment.'})
+    }
+
+}
