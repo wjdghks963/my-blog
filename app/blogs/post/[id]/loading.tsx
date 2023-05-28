5318ee34d303e0ed22dfda05445440fa350108c0
wjdghks963 <chsw000@naver.com>
2023-05-28 22:54:15 +0900

    added : - ssg page loading 할때 lottie 띄움

---
+++ app/blogs/post/[id]/loading.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+import Lottie from 'lottie-react'
+import LoadingJson from "../../../../public/lottie.json"
+
+export default function Loading(){
+    return <div>
+<Lottie animationData={LoadingJson} loop={true}/>
+    </div>
+}
