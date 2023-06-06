f294825a44ba963c2ab6a9288539bb0a09746c97
wjdghks963 <chsw000@naver.com>
2023-06-06 21:21:45 +0900

    modified : - table header에 따라 크기, weight 변경 - 스크롤에 따라 SlideHeaderNavBar, MarkdownContentTable visibility 설정 - 두 컴포넌트에 공통적으로 사용하는 스크롤 로직 함수 - 사용하지 않는 Loading 삭제

--- components/Base/NavBar/SlideHeaderNavBar.tsx
+++ components/Base/NavBar/SlideHeaderNavBar.tsx
@@ -2,27 +2,10 @@
 import {motion, useCycle, Variants} from "framer-motion";
 import MenuToggle from '@components/Base/NavBar/Toggle'
 import Navigation from '@components/Base/NavBar/Navigation'
+import useVisibleScrollY from '@libs/client/useVisibleScrollY'
+import {cls} from '@libs/client/utils'
 
-// const sidebar:Variants = {
-//     open: () => ({
-//
-//         transition: {
-//             type: "spring",
-//             stiffness: 20,
-//             restDelta: 2,
-//             duration: 0.8,
-//         }
-//     }),
-//     closed: {
-//         transition: {
-//             type: "spring",
-//             stiffness: 400,
-//             damping: 40,
-//             duration: 0.8,
-//         },
-//
-//     }
-// };
+
 const sidebar:Variants = {
     open: (height = 1000) => ({
         clipPath: `circle(${height * 2}px at 40px 40px)`,
@@ -45,11 +28,13 @@ const sidebar:Variants = {
 export default function SlideHeaderNavBar(){
     const [isOpen, toggleOpen] = useCycle(false, true);
 
+    const isVisible =  useVisibleScrollY()
+
     return  <motion.nav
         initial={false}
         animate={isOpen ? "open" : "closed"}
         variants={sidebar}
-        className={'fixed top-0 left-0 z-10 rounded-md'}
+        className={cls(isVisible ? "block" : "hidden",'fixed top-0 left-0 z-10 rounded-md')}
     >
         <motion.div className="absolute bg-white w-[200px] h-[100vh] " variants={sidebar} />
 
@@ -57,5 +42,3 @@ export default function SlideHeaderNavBar(){
         <Navigation isOpen={isOpen} toggleOpen={toggleOpen}/>
     </motion.nav>
 }
-
-
