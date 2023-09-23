9061c5a0cacf1f5b3b1ad63530b8450771429712
wjdghks963 <chsw000@naver.com>
2023-09-23 15:13:47 +0900

    bugfix : - 정규식 변경

--- libs/server/RegImageSrc.ts
+++ libs/server/RegImageSrc.ts
@@ -2,7 +2,11 @@ type ImageUrl = string | undefined;
 
 export const RegImageSrc = (data: ImageUrl) => {
   if (data === undefined || data === null) return null;
-  const findUrl = new RegExp(`!\[[^\]]*\]\((?<filename>.*?)(?=\"|\))(?<optionalpart>\".*\")?[)]`, "g");
-  const withArray = findUrl.exec(data);
-  return withArray?.groups!.filename.slice(1);
+
+  if (data) {
+    const imageRegex = /!\[([^\]]*)\]\(([^)]*)\)/;
+
+    // @ts-ignore
+    return data.match(imageRegex) ? data?.match(imageRegex)[2] : null;
+  }
 };
