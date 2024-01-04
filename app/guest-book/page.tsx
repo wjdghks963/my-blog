c317e3e918556d703bb9720aff09141d32b000de
wjdghks963 <chsw000@naver.com>
2024-01-04 19:55:36 +0900

    modified : - 단어 변경

--- app/guest-book/page.tsx
+++ app/guest-book/page.tsx
@@ -8,7 +8,7 @@ import LoadingJson from "@public/loadingLottie.json";
 export default function Blogs() {
   return (
     <div className={"h-screen"}>
-      <h1 className={"text-3xl font-bold text-center"}>준비 중..</h1>
+      <h1 className={"text-3xl font-bold text-center"}>개발 준비 중..</h1>
       <Lottie
         className={"h-full"}
         animationData={LoadingJson}
