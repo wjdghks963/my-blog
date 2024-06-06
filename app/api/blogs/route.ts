e0efd3846a5bb77c5daf85fc721aa36a1452ea6e
wjdghks963 <chsw000@naver.com>
2024-06-06 21:33:38 +0900

    modified : 1. 인피니티 스크롤 쿼리문 orm 사용 제거 2. 쿼리 검색시 제목과 내용을 포함한 결과를 나올 수 있도록 개선

--- app/api/blogs/route.ts
+++ app/api/blogs/route.ts
@@ -1,3 +1,4 @@
+import { PostWithId } from "@types";
 import { NextResponse } from "next/server";
 
 import prismaclient from "@libs/server/prismaClient";
@@ -11,66 +12,60 @@ export async function GET(request: Request) {
   const limit = searchParams.get("limit") !== "undefined" ? +searchParams.get("limit")! : 5;
 
   if (tag !== "all" && query === "") {
-    const posts = await prismaclient.post.findMany({
-      take: limit,
-      skip: (page - 1) * limit,
-      orderBy: {
-        createdAt: "desc",
-      },
-      where: {
-        tags: { some: { tag: { tag } } },
-      },
-      include: {
-        tags: {
-          select: {
-            tag: true,
-          },
-        },
-      },
-    });
+    const posts: PostWithId[] = await prismaclient.$queryRaw<PostWithId[]>`
+      SELECT 
+            p.*, 
+            COALESCE(json_agg(json_build_object('tag', t.tag)) FILTER (WHERE t.tag IS NOT NULL), '[]'::json) AS tags
+      FROM 
+            "Post"  p
+      LEFT JOIN 
+            "PostTag" pt ON p.id = pt."postId"
+      LEFT JOIN 
+            "Tag" t ON t.id = pt."tagId"
+      WHERE 
+            t.tag = ${tag}
+      GROUP BY p.id
+      ORDER BY p."createdAt" DESC
+      LIMIT ${limit} OFFSET ${(page - 1) * limit};
+`;
+    console.log(posts);
 
     const hasNextPage = posts.length === limit;
     return NextResponse.json({ data: posts, hasNextPage });
   }
 
+  // 제목과 내용 중에 맞는 것이 있다면 전부 나오게 함
   if (query !== "all" && tag === "") {
-    const posts = await prismaclient.post.findMany({
-      take: limit,
-      orderBy: {
-        createdAt: "desc",
-      },
-      where: {
-        title: {
-          contains: query,
-        },
-      },
-      include: {
-        tags: {
-          select: {
-            tag: true,
-          },
-        },
-      },
-    });
+    const posts: PostWithId[] = await prismaclient.$queryRaw<PostWithId[]>`
+    SELECT 
+        p.*, 
+        COALESCE(json_agg(json_build_object('tag', t.tag)) FILTER (WHERE t.tag IS NOT NULL), '[]'::json) AS tags
+    FROM 
+        "Post" p
+    LEFT JOIN 
+        "PostTag" AS pt ON p.id = pt."postId"
+    LEFT JOIN 
+        "Tag" AS t ON t.id = pt."tagId"
+    WHERE 
+        p.title ILIKE ${`%${query}%`} OR p.content ILIKE ${`%${query}%`}
+    GROUP BY p.id
+    ORDER BY p."createdAt" DESC
+    LIMIT ${limit} OFFSET ${(page - 1) * limit};
+    `;
 
     const hasNextPage = posts.length === limit;
     return NextResponse.json({ data: posts, hasNextPage });
   }
 
-  const allPosts = await prismaclient.post.findMany({
-    take: limit,
-    skip: (page - 1) * limit,
-    orderBy: {
-      createdAt: "desc",
-    },
-    include: {
-      tags: {
-        select: {
-          tag: true,
-        },
-      },
-    },
-  });
+  const allPosts: PostWithId[] = await prismaclient.$queryRaw<PostWithId[]>`
+      SELECT p.*, COALESCE(json_agg(json_build_object('tag', t.tag)) FILTER (WHERE t.tag IS NOT NULL), '[]'::json) AS tags
+      FROM "Post" p
+      LEFT JOIN "PostTag" pt ON p.id = pt."postId"
+      LEFT JOIN "Tag" t ON t.id = pt."tagId"
+      GROUP BY p.id
+      ORDER BY p."createdAt" DESC
+      LIMIT ${limit} OFFSET ${(page - 1) * limit};
+`;
 
   const hasNextPage = allPosts.length === limit;
 
