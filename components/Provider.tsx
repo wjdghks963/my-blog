06252c17d476d37fc9a294bcb3aa99470cfbafd2
wjdghks963 <chsw000@naver.com>
2025-01-22 21:52:50 +0900

    테스트

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -1,24 +1,44 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { isServer, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
-import { ReactNode, useState } from "react";
+import React, { ReactNode } from "react";
 import { Provider as ReduxProvider } from "react-redux";
 
 import { makeStore } from "@store/index";
 
-export default function Provider({ children }: { children: ReactNode }) {
-  const queryClient = new QueryClient({
+function makeQueryClient() {
+  return new QueryClient({
     defaultOptions: {
       queries: {
+        // With SSR, we usually want to set some default staleTime
+        // above 0 to avoid refetching immediately on the client
+        staleTime: 60 * 1000,
         //@ts-ignore
         suspense: true,
       },
     },
   });
+}
+let browserQueryClient: QueryClient | undefined = undefined;
+function getQueryClient() {
+  if (isServer) {
+    // Server: always make a new query client
+    return makeQueryClient();
+  } else {
+    // Browser: make a new query client if we don't already have one
+    // This is very important, so we don't re-make a new client if React
+    // suspends during the initial render. This may not be needed if we
+    // have a suspense boundary BELOW the creation of the query client
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+export default function Provider({ children }: { children: ReactNode }) {
   const store = makeStore();
+  const queryClient = getQueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
